3b6c78f01cf04447d6160776fb75e39ddc2684e0
Camila Mandai <cayumandai@gmail.com>
2022-08-09 16:33:31 -0300

    implementa requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,22 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('is a function', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('calls fetch', async() => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('call fetch with the right endpoint', async() => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+  it('returns an object equal to "item"', async() => {
+    const result = await fetchItem('MLB1615760527');
+    expect(result).toEqual(item);
+  });
+  it('returns an error if the function is empty', async () => {
+    await expect(async () => await fetchItem()).toThrow(new Error('You must provide an url'));
+  });
 });
