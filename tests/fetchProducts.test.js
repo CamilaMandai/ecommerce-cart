5d2c5186950b5019929bff25c85b538a357389ae
Camila Mandai <cayumandai@gmail.com>
2022-08-09 15:29:44 -0300

    implementa requisito 1?

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,5 +5,19 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fetchProducts', () => {
   it('is a function', () => {
     expect(typeof fetchProducts).toBe('function');
+  });
+  it('calls fetch', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('calls fetch with endpoint', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it('returns the object computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+  it('returns an error if the function is empty', async () => {
+    await expect(async () => await fetchProducts()).toThrow(new Error('You must provide an url'));
   })
 });
