94dcec5c016ff06dc0db2ca5753e312eb6aa4d4d
Camila Mandai <cayumandai@gmail.com>
2022-08-10 15:15:50 -0300

    implementa requisito 9, mas com um erro de lint (await dentro de loop)

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 let savedItems = JSON.parse(getSavedCartItems('cartItems'));
 const cartItems = document.getElementsByClassName('cart__items')[0];
 const emptyCart = document.querySelector('.empty-cart');
+const totalPrice = document.querySelector('.total-price');
 
-emptyCart.addEventListener('click', () => {
+const calculaTotal = async () => {
+    let total = 0;
+    const items = JSON.parse(getSavedCartItems('cartItems'));
+    // const total = items.reduce( async (acc, curr) => {
+    //   const {sku} = curr;
+    //   const item = await fetchItem(sku);
+    //   console.log(acc + item.price)
+    //   return acc + item.price;
+    // })
+    for (let index = 0; index < items.length; index += 1) {
+      const { sku } = items[index];
+      const item = await fetchItem(sku);
+      total += item.price;
+    }
+    totalPrice.innerText = `${total}`;
+  };
+
+const clearAll = () => {
   localStorage.clear();
-  // window.location.reload();
+  savedItems = [];
   while (cartItems.firstChild) {
     cartItems.removeChild(cartItems.firstChild);
   }
-});
+  totalPrice.innerText = 0;
+};
+
+emptyCart.addEventListener('click', clearAll);
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -48,9 +69,20 @@ const listProducts = async () => {
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const cartItemClickListener = (event) => {
+const cartItemClickListener = async (event) => {
   const item = event.target;
+  const sku = item.innerText.split(' ')[1];
+  const indexRemove = savedItems.map((element, index) => {
+    if (element.sku === sku) {
+      return index;
+    }
+    return undefined;
+  }).find((element) => element !== undefined);
+  // console.log(indexRemove)
+  savedItems.splice(indexRemove, 1);
+  saveCartItems(savedItems);
   item.remove();
+  await calculaTotal();
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
@@ -64,13 +96,15 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
 const fetchById = (id) => fetchItem(id);
 
 const pushAndSaveItem = (item) => {
+  // localItems = JSON.parse(getSavedCartItems('cartItems'));
   savedItems.push(item);
   saveCartItems(savedItems);
 };
 
-const addItem = () => {
+const addItem = async () => {
   const buttons = document.getElementsByClassName('item__add');
   for (let i = 0; i < buttons.length; i += 1) {
+    // savedItems = JSON.parse(getSavedCartItems('cartItems'));
     const itemId = buttons[i].parentElement.firstChild.innerText;
     buttons[i].addEventListener('click', async () => {
       const results = await fetchById(itemId);
@@ -78,20 +112,21 @@ const addItem = () => {
       const productCart = createCartItemElement({ sku, name, salePrice });
       cartItems.appendChild(productCart);
       pushAndSaveItem({ sku, name, salePrice });
+      await calculaTotal();
     });
   }
 };
 
-const addSavedItems = () => {
-  // const [temp] = savedItems;
+const addSavedItems = async () => {
   if (!savedItems) {
     savedItems = [];
   } else {
     savedItems.forEach((element) => {
-    const { sku, name, salePrice } = element;
-    const productCart = createCartItemElement({ sku, name, salePrice });
-    cartItems.appendChild(productCart);
+      const { sku, name, salePrice } = element;
+      const productCart = createCartItemElement({ sku, name, salePrice });
+      cartItems.appendChild(productCart);
     });
+    await calculaTotal();
   }
 };
 
@@ -99,4 +134,5 @@ window.onload = async () => {
   await listProducts();
   addSavedItems();
   addItem();
+  await calculaTotal();
 };
